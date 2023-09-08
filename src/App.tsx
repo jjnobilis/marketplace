12ee537fa1d95bfa599d9c539ebc5b6ce1b8e71f
Nobilis <jgnangnelasme@gmail.com>
2023-09-08 22:04:52 +0000

    Template Product List

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { ProductCard } from "./components/products/ProductCard";
+import { products } from "./data/_data";
+import { ProductGrid } from "./components/products/ProductGrid";
 
-function App() {
-  return (
-    <>
-      <Button colorScheme="blue">MyButton</Button>
-    </>
-  );
-}
-
+export const App = () => (
+  <Box
+    maxW="7xl"
+    mx="auto"
+    px={{ base: "4", md: "8", lg: "12" }}
+    py={{ base: "6", md: "8", lg: "12" }}
+  >
+    <ProductGrid>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </ProductGrid>
+  </Box>
+);
 export default App;
