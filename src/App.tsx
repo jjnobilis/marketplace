ecc4281513a6a1c186fdb47fd53dab51c89aaa47
Nobilis <jgnangnelasme@gmail.com>
2023-09-08 13:12:43 +0000

    Premier Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Button colorScheme="blue">Nobilis Button</Button>
     </>
   );
 }
